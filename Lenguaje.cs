3ba063fdda10908cbffa31380b07e9e7ab261881
CayetanaNevado <cayetananevado321@gmail.com>
2022-11-22 16:20:09 -0600

    Prueba

---
+++ Lenguaje.cs
@@ -0,0 +1,42 @@
+//MARIA DE LOS ANGELES CAYETANA NEVADO RODRIGUEZ
+using System;
+using System.Collections.Generic;
+
+namespace Generador
+{
+    public class Lenguaje : Sintaxis, IDisposable
+    {
+        public Lenguaje(string nombre) : base(nombre)
+        {
+        }
+        public Lenguaje()
+        {
+        }
+        public void Dispose()
+        {
+            cerrar();
+        }
+        public void gramatica()
+        {
+            cabecera();
+            listaProducciones();
+        }
+        private void cabecera()
+        {
+            match("Gramatica");
+            match(":");
+            match(Tipos.SNT);
+            match(Tipos.FinProduccion);
+        }
+        private void listaProducciones()
+        {
+            match(Tipos.SNT);
+            match(Tipos.Produce);
+            match(Tipos.FinProduccion);
+            if(!FinArchivo())
+            {
+                listaProducciones();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿//MARIA DE LOS ANGELES CAYETANA NEVADO RODRIGUEZ
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Generador
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                using (Lenguaje a = new Lenguaje("c2.gram"))
+                
+                {
+                    a.gramatica();
+                    /*
+                    while(!a.FinArchivo())
+                    {
+                        a.NextToken();
+                    }
+                    */
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
